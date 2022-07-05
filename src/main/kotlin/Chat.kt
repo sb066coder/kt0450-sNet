c48078a16a4aa6ed3146da0530ac1f95e14bb23b
Sergey Buksha <sergey066@yandex.ru>
2022-07-05 16:37:04 +0300

    ChatService Optimization

--- src/main/kotlin/Chat.kt
+++ src/main/kotlin/Chat.kt
@@ -1,13 +1,9 @@
 data class Chat(
-    val id: Int,
-    var ownersId: Pair<Int, Int>,
-    val messages: MutableList<Message> = mutableListOf(),
+    val messages: MutableMap<Int, Message> = mutableMapOf(),
     var lastMessageId: Int = -1
 )
 
 data class Message(
-    val id: Int,
-    val chatId: Int,
     val senderId: Int,
     var text: String,
     var isRead: Boolean = false,
--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -2,4 +2,8 @@ class User (
     val id: Int,
     val name: String = "Unknown",
     var age: Int = 18
-)
+): Comparable<User> {
+    override fun compareTo(other: User): Int {
+        return this.id - other.id
+    }
+}
